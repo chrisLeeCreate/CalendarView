c39f40eb8ceb29c9ac1c77b27832cb694a43fad5
李少伟 <lishaowei@lishaoweideiMac.local>
2016-12-10 13:04:38 +0800

    greenDao

--- app/src/main/java/com/boxfish/lsw/calendarview/MainActivity.java
+++ app/src/main/java/com/boxfish/lsw/calendarview/MainActivity.java
@@ -1,7 +1,9 @@
 package com.boxfish.lsw.calendarview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 import com.boxfish.lsw.c.CalendarView;
@@ -16,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
         calendarView.setStartByNowMonth(5);
         calendarView.setOnCanlendarDayClickListener(scheduleDate -> Toast.makeText(getApplicationContext(), scheduleDate.dateTime.toString(), Toast.LENGTH_SHORT).show()
         );
+    }
+
+    public void gotogreendao(View v) {
+        Intent intent = new Intent(this, TextNoteActivity.class);
+        startActivity(intent);
     }
 }
---
+++ daoexamplegenerator/src/main/java/com/example/ExampleDaoGenerator.java
@@ -0,0 +1,23 @@
+package com.example;
+
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+public class ExampleDaoGenerator {
+    public static void main(String[] args) throws Exception {
+        Schema schema = new Schema(1, "com.boxfish.lishaowei");
+        addNote(schema);
+        //添加实体类
+        new DaoGenerator().generateAll(schema, "/Users/lishaowei/StudioProjects/CalendarView/app/src/main/java-gen");
+    }
+    private static void addNote(Schema schema) {
+        Entity note = schema.addEntity("Note");
+
+        note.addIdProperty();
+        note.addStringProperty("text").notNull();
+        note.addStringProperty("comment");
+        note.addDateProperty("date");
+    }
+}
